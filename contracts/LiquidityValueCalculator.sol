495d3fc1e2f7792b6f9259440111005d1c3f6bc6
BTC415 <alenschmitt415@gmail.com>
2024-10-27 21:07:59 +0000

    [add] added pairInfo func

--- contracts/LiquidityValueCalculator.sol
+++ contracts/LiquidityValueCalculator.sol
@@ -2,17 +2,35 @@
 pragma solidity ^0.8.27;
 
 import "./interface/ILiquidityValueCalculator.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol";
 
 contract LiquidityValueCalculator is ILiquidityValueCalculator {
+    address public factory;
+    constructor(address factory_) {
+        factory = factory_;
+    }
 
-  address public factory;
-  constructor(address factory_) {
-    factory = factory_;
-  }
+    function pairInfo(
+        address tokenA,
+        address tokenB
+    )
+        internal
+        view
+        returns (uint reserveA, uint reserveB, uint totalLiquidity)
+    {
+        IUniswapV2Pair pair = IUniswapV2Pair(
+            UniswapV2Library.pairFor(factory, tokenA, tokenB)
+        );
+        (uint reserve0, uint reserve1, ) = pair.getReserves();
+        (reserveA, reserveB) = tokenA < tokenB ? (reserve0, reserve1) : (reserve1, reserve0);
+        totalLiquidity = pair.totalSupply();
+    }
 
-  function pairInfo(address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB, uint totalLiquidity) {
-    
-  }
-
-  function computeLiquidityShareValue(uint liquidity, address tokenA, address tokenB) external view returns (uint tokenAAmount, uint tokenBAmount) {}
+    function computeLiquidityShareValue(
+        uint liquidity,
+        address tokenA,
+        address tokenB
+    ) external view returns (uint tokenAAmount, uint tokenBAmount) {
+    }
 }
