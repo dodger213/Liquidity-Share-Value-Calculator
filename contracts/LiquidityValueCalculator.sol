6733ac2f1d8e06bd246b0bfb88cb5e28e6a7eb21
BTC415 <alenschmitt415@gmail.com>
2024-10-27 20:49:48 +0000

    [add] added func structure

--- contracts/LiquidityValueCalculator.sol
+++ contracts/LiquidityValueCalculator.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.8.27;
 
 import "./interface/ILiquidityValueCalculator.sol";
 
-contract LiquidityValueCalculator is ILiquidityValueCalculator {}
+contract LiquidityValueCalculator is ILiquidityValueCalculator {
+
+  function pairInfo(address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB, uint totalLiquidity) {}
+
+  function computeLiquidityShareValue(uint liquidity, address tokenA, address tokenB) external view returns (uint tokenAAmount, uint tokenBAmount) {}
+}
